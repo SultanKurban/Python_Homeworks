a1cef37443dd30ac5ed182b0d29afb30b763991c
SultanKurban <abstrakt88@gmail.com>
2025-07-13 01:04:11 +0500

    Update homework-1.py

--- lesson-1/homework/homework-1.py
+++ lesson-1/homework/homework-1.py
@@ -1,1 +1,36 @@
+# 1. Given a side of square. Find its perimeter and area.
 
+a = 10
+b = 5
+P = 2 * (a + b)
+S = a * b
+print (P) 
+print (S)
+
+# 2. Given diameter of circle. Find its length.
+import math
+
+D = 14 
+R = D / 2
+C = 2*(math.pi)*R
+print (C)
+
+# 3. Given two numbers a and b. Find their mean.
+
+a = 15
+b = 20
+mean = (a + b) / 2
+print (mean)
+
+# 4. Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 7
+b = 6
+sum = a + b 
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+print (sum)
+print (product)
+print (square_a)
+print (square_b)
